dcbdc7611599474b25ebf8e9922f8492e28460b9
Sofia <kulchitskasofia@gmail.com>
2022-11-22 05:58:18 +0200

    laba5.5

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../laba5.5/laba5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+        TEST_METHOD(TestMethod1)
+        {
+            cout << "binarty = " << to_binary_string(5) << endl;
+            Assert::AreEqual(to_binary_string(5), to_binary_string(5));
+
+        }
+        TEST_METHOD(TestMethod2)
+        {
+            cout << "eight = " << octal_string(6) << endl;
+            Assert::AreEqual(octal_string(6), octal_string(6));
+
+        }
+
+    };
+}
